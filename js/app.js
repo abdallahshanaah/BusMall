bf63d166ffb09ee7996dba869d1366cdeeba5c90
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-03-07 15:01:29 +0200

    edit all file

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 var allimage = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
-var leftimage = document.querySelector('#left-img');
-var middleimage = document.querySelector('#middle-img');
-var rightimage = document.querySelector('#right-img');
-var imhgroup = document.getElementById('allbusmall');
+var leftimage = document.querySelector('#left-img');// TO GET THE LEFT IMG FROM THE FORM IN HTML
+var middleimage = document.querySelector('#middle-img');// TO GET THE MIDDLE IMG FROM THE FORM IN HTML
+var rightimage = document.querySelector('#right-img');// TO GET THE RIGHT IMG FROM THE FORM IN HTML
+var imhgroup = document.getElementById('allbusmall');// TO GET THE SECTION BY ID FROM THE IN HTML
 var photos = []; // object array
 var storage = [];//for local starage
-var totalClicks = 0;
-var leftImgRand;
-var middleImgRand;
-var rightImgRand;
-var notrepeat=[];
+var totalClicks = 0;// THE TOTAL CLICK FOR THE ALL IMAGE NUMBER 
+var leftImgRand;// THE RANDOM LEFT IMG THAT THE FUNCTION WITH SECLECT
+var middleImgRand;// THE RANDOM MIDDLE IMG THAT THE FUNCTION WITH SECLECT
+var rightImgRand;// THE RANDOM RIGHT IMG THAT THE FUNCTION WITH SECLECT
+var notrepeat=[]; // ARRAY OF NOT  TEPEAT IMG IN THE WEBSITE
 
-
+// THE CUNSTRUCTRE FUNCTION 
 function Imagegame(name) {
     this.name = name;
     this.imagelink = `img/${this.name}`;
@@ -20,7 +20,7 @@ function Imagegame(name) {
     photos.push(this);
     storage.push(this);
 }
-
+// FOR TO CALL THE ALL OBJECT 
 for (let a = 0; a < allimage.length; a++) {
     new Imagegame(allimage[a]);
      setItem();
@@ -30,12 +30,13 @@ for (let a = 0; a < allimage.length; a++) {
 
 
 
-
+// FUNCTION TO PAKE RANDOM IMG 
 function RandomImages() {
     leftImgRand = photos[randomNumber(0, photos.length - 1)];
     middleImgRand = photos[randomNumber(0, photos.length - 1)];
     rightImgRand = photos[randomNumber(0, photos.length - 1)];
 
+// TO CHECK THE THE ALL IMG IS DEFFERENET 
     while ((leftImgRand.name === middleImgRand.name) || (leftImgRand.name === rightImgRand.name) || (middleImgRand.name === rightImgRand.name)||notrepeat.includes(leftImgRand) || notrepeat.includes(middleImgRand) || notrepeat.includes(rightImgRand)) {
         console.log('first while ')
         leftImgRand = photos[randomNumber(0, photos.length - 1)];
@@ -63,7 +64,7 @@ RandomImages();
 
 imhgroup.addEventListener('click', clickImage);
 //console.log(imhgroup);
-
+// FUNCTION TO ACTIVE THE CLICK THE THE IMG 
 function clickImage(img) {
 
     if (img.target.id === 'left-img') {
@@ -99,12 +100,12 @@ function clickImage(img) {
     }
      result_of_canves();
 }
-
+// TO SET IN THE LOCIAL STORAGE 
 function setItem(){
     var order = JSON.stringify(storage);
   localStorage.setItem( 'storage', order);
 }
-
+// TO GET FROM THE LOCIAL STORAGE 
 function getItem(){
     var storageOrders = localStorage.getItem('storageOrders');
     storage = JSON.parse(storageOrders);
@@ -115,7 +116,7 @@ RandomImages();
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+// TO ACTIVET THE CHART.JS WITH THE VIEW AND VOTES
 function result_of_canves() {
     var imgnames=[];
     var imhviews=[];
@@ -129,7 +130,7 @@ function result_of_canves() {
         imgvotes.push(imgvote);
    }
    
-
+// CHART FUNCTION 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
